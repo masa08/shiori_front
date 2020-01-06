4f0df6ce08780ce11f6bc937db022efb3229a109
masa08 <ushimasarug08@gmail.com>
2020-01-06 22:52:12 +0900

    :arrow_up: Add vue-router

---
+++ src/components/commons/Header.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app-bar
+    app
+    color="#18bc9c"
+    dark
+  >
+    <div class="d-flex align-center">
+      <h1>SHIORI</h1>
+    </div>
+
+    <v-spacer></v-spacer>
+
+    <v-btn
+      href="https://github.com/vuetifyjs/vuetify/releases/latest"
+      target="_blank"
+      text
+    >
+      <span class="mr-2">ログイン</span>
+      <span class="mr-2">新規登録</span>
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+
+  data: () => ({
+
+  }),
+}
+</script>
