378adecf15ae0702032b732c7eb3b64a5ca94d8d
masa08 <ushimasarug08@gmail.com>
2020-01-23 18:55:06 +0900

    :art: Change ui in mainimage

--- src/components/organisms/MainImage.vue
+++ src/components/organisms/MainImage.vue
@@ -1,29 +1,41 @@
 <template>
   <div class="main-image">
     <v-container>
-      <v-row>
-        <v-col class="main-image__left" cols="9">
-          <div class="main-image__message">
-            <h2>SHIORI</h2>
-            <p>心に響く文章と出会う場所</p>
-          </div>
-        </v-col>
-        <v-col class="main-image__right" cols="3">
-          <div class="main-image__register">
-            <h2>新規登録</h2>
-            <router-link to="/register">
-              <Button class="main-image__btn" title="メールアドレスで登録" btnColor="success" />
-            </router-link>
-            <!-- <Button class="main-image__btn" title="Twitterで登録" btnColor="primary" /> -->
-          </div>
-          <div class="main-image__login">
-            <h2>ログイン</h2>
-            <router-link to="/login">
-              <Button class="main-image__btn" title="ログインはこちら" btnColor="" />
-            </router-link>
-          </div>
-        </v-col>
-      </v-row>
+      <div v-if="this.$store.state.token">
+        <v-row>
+          <v-col class="main-image__left" cols="12">
+            <div class="main-image__message">
+              <h2>SHIORI</h2>
+              <p>心に響く文章と出会う場所</p>
+            </div>
+          </v-col>
+        </v-row>
+      </div>
+      <div v-else>
+        <v-row>
+          <v-col class="main-image__left" cols="9">
+            <div class="main-image__message">
+              <h2>SHIORI</h2>
+              <p>心に響く文章と出会う場所</p>
+            </div>
+          </v-col>
+          <v-col class="main-image__right" cols="3">
+            <div class="main-image__register">
+              <h2>新規登録</h2>
+              <router-link to="/register">
+                <Button class="main-image__btn" title="メールアドレスで登録" btnColor="success" />
+              </router-link>
+              <!-- <Button class="main-image__btn" title="Twitterで登録" btnColor="primary" /> -->
+            </div>
+            <div class="main-image__login">
+              <h2>ログイン</h2>
+              <router-link to="/login">
+                <Button class="main-image__btn" title="ログインはこちら" btnColor="" />
+              </router-link>
+            </div>
+          </v-col>
+        </v-row>
+      </div>
     </v-container>
   </div>
 </template>
