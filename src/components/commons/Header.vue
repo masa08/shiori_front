31de2b6c163a0e770bad184eb51b54d569abad00
masa08 <ushimasarug08@gmail.com>
2021-03-04 22:22:31 +0900

    :sparkles: Add pc hamburger

--- src/components/commons/Header.vue
+++ src/components/commons/Header.vue
@@ -1,56 +1,110 @@
 <template>
-  <v-app-bar
-    app
-    color="#fff"
-  >
-    <div class="d-flex align-center">
-      <h1><router-link to="/" style="color:#18bc9c;">SHIORI</router-link></h1>
-    </div>
-    <v-spacer></v-spacer>
-    <div class="login" v-if="this.$store.state.token">
-      <router-link to="/search">
-        <v-btn
-          target="_blank"
-          text
-        >
-          <span class="mr-2">本を検索</span>
-        </v-btn>
-      </router-link>
-      <router-link to="/user/2">
-        <v-btn
-          target="_blank"
-          text
-        >
-          <span class="mr-2">マイページ</span>
-        </v-btn>
-      </router-link>
-      <v-btn
-        target="_blank"
-        text
-        v-on:click="logout"
+  <div class="mx-auto overflow-hidden">
+    <v-app-bar app color="#fff">
+      <div class="d-flex align-center">
+        <h1><router-link to="/" style="color:#18bc9c;">SHIORI</router-link></h1>
+      </div>
+      <v-spacer></v-spacer>
+      <v-app-bar-nav-icon @click="drawer=!drawer"></v-app-bar-nav-icon>
+    </v-app-bar>
+    <v-navigation-drawer
+      v-if="this.$store.state.token"
+      v-model="drawer"
+      absolute
+      bottom
+      temporary
+      right
+      fixed
+    >
+      <v-list
+        nav
+        dense
       >
-        <span class="mr-2">ログアウト</span>
-      </v-btn>
-    </div>
-    <div v-else>
-      <router-link to="/login">
-        <v-btn
-          target="_blank"
-          text
+        <v-list-item-group
+          v-model="group"
         >
-          <span class="mr-2">ログイン</span>
-        </v-btn>
-      </router-link>
-      <router-link to="/register">
-        <v-btn
-          target="_blank"
-          text
-        >
-          <span class="mr-2">新規登録</span>
-        </v-btn>
-      </router-link>
-    </div>
-  </v-app-bar>
+          <v-list-item>
+            <v-list-item-title>
+              <router-link to="/search">
+                <v-btn
+                  target="_blank"
+                  text
+                >
+                  <span class="mr-2">本を検索</span>
+                </v-btn>
+              </router-link>
+            </v-list-item-title>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-title>
+              <router-link to="/user/2">
+                <v-btn
+                  target="_blank"
+                  text
+                >
+                  <span class="mr-2">マイページ</span>
+                </v-btn>
+              </router-link>
+            </v-list-item-title>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-title>
+              <v-btn
+                target="_blank"
+                text
+                v-on:click="logout"
+              >
+                <span class="mr-2">ログアウト</span>
+              </v-btn>
+            </v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+    <v-navigation-drawer
+      v-else
+      v-model="drawer"
+      absolute
+      bottom
+      temporary
+      right
+      fixed
+    >
+      <v-list
+        nav
+        dense
+      >
+      <v-list-item-group
+        v-model="group"
+      >
+        <v-list-item>
+          <v-list-item-title>
+            <router-link to="/login">
+              <v-btn
+                target="_blank"
+                text
+              >
+                <span class="mr-2">ログイン</span>
+              </v-btn>
+            </router-link>
+          </v-list-item-title>
+        </v-list-item>
+        <v-list-item>
+          <v-list-item-title>
+        <router-link to="/register">
+          <v-btn
+            target="_blank"
+            text
+          >
+            <span class="mr-2">新規登録</span>
+          </v-btn>
+        </router-link>
+          </v-list-item-title>
+        </v-list-item>
+      </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
 </template>
 
 <script>
@@ -60,8 +114,14 @@ export default {
   name: 'Header',
 
   data: () => ({
-
+    drawer: false,
+    group: null,
   }),
+  watch: {
+    group () {
+      this.drawer = false
+    },
+  },
   methods: {
     async logout() {
       await firebase.auth().signOut();
